94cc6d74883b48f1017d3baf15ca125153c9a3eb
CHonesetDoPa <ch@nekoc.cc>
2023-05-06 00:10:42 +0800

    这次应该没问题了吧

--- assets/js/locate-en.js
+++ assets/js/locate-en.js
@@ -4,14 +4,14 @@ $(function () {
         var dataid = $(this).attr('data-id');
         if (dataid == "app4") {
             $(".shang_payimg a").attr("href", "https://patreon.com/CHonesetDoPa");
-            $(".shang_payimg img").attr("title", "点击前往Patreon");
+            $(".shang_payimg img").attr("title", "Go to Patreon");
             document.getElementById("info-text-0001").innerHTML = "Click this Patreon Logo.";
         } else {
             $(".shang_payimg a").attr("href", "#");
-            $(".shang_payimg img").attr("title", "扫一扫");
+            $(".shang_payimg img").attr("title", "Scan");
             document.getElementById("info-text-0001").innerHTML = "Scan this QR Code.";
         }
-        $(".shang_payimg img").attr("src", "assets/images/sponsor/" + dataid + ".png");
+        $(".shang_payimg img").attr("src", "../assets/images/sponsor/" + dataid + ".png");
     });
 });
 function dashangToggle() {
